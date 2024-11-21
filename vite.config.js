4ce4b1996dfdb0b3b2d3e20e8bdd60043058591d
efs-multiconsult <efs@multiconsult.no>
2024-11-21 21:53:49 +0100

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ol-map/', // Replace with your repository name
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
